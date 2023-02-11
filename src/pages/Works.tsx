e1e9b462f53c4fb882bdf30634dbb36d0660a8a6
yaberah <yabe.masato.ly@alumni.tsukuba.ac.jp>
2023-02-11 17:01:30 +0900

    Add contentful

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react';
+import getPosts from '@/features/getPosts';
+
+const { getCollection } = getPosts();
+ 
 const Works: React.FC = () => {
 
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    getCollection().then(posts => setPosts(posts))
+  },[])
+  
   return (
     <>
       <p>Works</p>
+        {
+          posts.map(post => (
+            <div key={post.id}>{post.title}</div>
+          ))
+        }
     </>
   );
 };
