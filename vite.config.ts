5cdb3fd2d4630ec1f3ed4a4670c3464b884e9b2d
yaberah <yabe.masato.ly@alumni.tsukuba.ac.jp>
2023-02-19 15:58:20 +0900

    Add emotion

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    tsconfigPaths()
+  ],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 });
